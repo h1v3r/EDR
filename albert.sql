6974a90bcf2b36f092b71ae99ba163b3aa01f31b
Albert <wi18b088@technikum-wien.at>
2020-06-19 10:04:03 +0200

    Datei für view_user_produkte erstellt

---
+++ albert.sql
@@ -0,0 +1,7 @@
+/*********************************************************************
+/**
+/** Table (View): view_user_produkte
+/** Developer: Albert Schleidt
+/** Description: Lists all products, a user is currently offering
+/**
+/*********************************************************************/
