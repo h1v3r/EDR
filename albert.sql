2c903295fc9be5cd23f2f5b886349768dc5c665e
Albert <wi18b088@technikum-wien.at>
2020-06-19 11:41:03 +0200

    filter_angebot_by_produktname hinzugefügt

--- albert.sql
+++ albert.sql
@@ -11,3 +11,22 @@ create or replace view view_user_produkte as
     join "Produkt_Angebot" using ("produktid")
     join "Angebot" using ("angebotid")
     join "User" on "userid_verkaeufer" = "userid";
+
+
+/*********************************************************************
+/**
+/** Procedure filter_angebot_by_produktname
+/** Out: nothing
+/** In: p_suchstring_in
+/** Developer: Albert Schleidt
+/** Description: Takes an input string, shows every Angebot that offers a Produkt containing the string.
+/**
+/*********************************************************************/
+create or replace function filter_angebot_by_produktname (p_suchstring_in in varchar) return SYS_REFCURSOR as
+    return_cursor SYS_REFCURSOR;
+begin
+    open return_cursor for select * from "Angebot" join "Produkt_Angebot" using ("angebotid") join "Produkt" using ("produktid") where "name" like '%'||p_suchstring_in||'%';
+    return return_cursor;
+--exception
+end;
+/
