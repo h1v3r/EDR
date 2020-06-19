7b3463bc4d3086fa71435c961395b11b45bb551e
Albert <wi18b088@technikum-wien.at>
2020-06-19 10:11:16 +0200

    view_user_produkte erstellt

--- albert.sql
+++ albert.sql
@@ -2,6 +2,12 @@
 /**
 /** Table (View): view_user_produkte
 /** Developer: Albert Schleidt
-/** Description: Lists all products, a user is currently offering
+/** Description: Lists all products, users are currently offering
 /**
 /*********************************************************************/
+create or replace view view_user_produkte as
+    select "angebotid" as "Angebot ID", "produktid" as "Produkt ID", "name" as "Produktname", "userid_verkaeufer" as "User ID Verkäufer", "vorname" "Vorname", "nachname" as "Nachname"
+    from "Produkt"
+    join "Produkt_Angebot" using ("produktid")
+    join "Angebot" using ("angebotid")
+    join "User" on "userid_verkaeufer" = "userid";
