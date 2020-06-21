6f7b57b60743a0498301317929556cbfd196bd73
Albert <wi18b088@technikum-wien.at>
2020-06-21 12:46:33 +0200

    added procedure add_adresse

--- albert.sql
+++ albert.sql
@@ -22,7 +22,7 @@ CREATE OR REPLACE view view_user_produkte AS
 /** Description: Takes an input string, shows every Angebot that offers a Produkt containing the string. Returns a SYS_REFCURSOR.
 /**
 /*********************************************************************/
-CREATE OR REPLACE FUNCTION filter_angebot_by_produktname (l_v_suchstring_in IN VARCHAR) RETURN SYS_REFCURSOR , l_n_error_out OUT NUMBERAS
+CREATE OR REPLACE FUNCTION filter_angebot_by_produktname (l_v_suchstring_in IN VARCHAR) RETURN SYS_REFCURSOR AS
     l_return_cursor_cur_out SYS_REFCURSOR;
 BEGIN
     OPEN l_return_cursor_cur_out for SELECT * FROM "Angebot" JOIN "Produkt_Angebot" USING ("angebotid") JOIN "Produkt" USING ("produktid") WHERE "name" LIKE '%'||l_v_suchstring_in||'%';
@@ -43,7 +43,6 @@ END;
 /** Description: Creates a new Ort, IF it doesn't exist already.
 /**
 /*********************************************************************/
--- Entweder mit OUT Parameter Fehlercode machen oder FUNCTION RETURN NUMBER den PK des erzeugten Eintrags
 CREATE OR REPLACE PROCEDURE add_ort (l_n_plz_in IN NUMBER, l_v_ortsname_in IN VARCHAR, l_n_pk_out OUT NUMBER, l_n_error_out OUT NUMBER) AS
     l_n_countEntries NUMBER;
 BEGIN
@@ -60,4 +59,41 @@ EXCEPTION
         l_n_error_out := SQLCODE;
 END;
 /
+
+/*********************************************************************
+/**
+/** Procedure: add_adresse
+/** Out: Primary Key ID of the existing or added element
+/** Out: Error code if error occured
+/** In: l_n_plzpk_in - the primary key of the plz to be associated with the Adresse
+/** In: l_v_strasse_in - the Strasse parameter to be added to the Adresse
+/** In: l_v_hausnummer_in - the Hausnummer parameter to be added to the Adresse
+/** Developer: Albert Schleidt
+/** Description: Creates a new Adresse, if it doesn't exist already.
+/**
+/*********************************************************************/
+CREATE OR REPLACE PROCEDURE add_adresse (l_n_plzpk_in IN NUMBER, l_v_strasse_in IN VARCHAR, l_v_hausnummer_in IN VARCHAR, l_n_pk_out OUT NUMBER, l_n_error_out OUT NUMBER) AS
+    l_n_countEntries NUMBER;
+    l_n_adresseid NUMBER;
+    l_n_maxAdresseid NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO l_n_countEntries FROM "Adresse" WHERE "plz" = l_n_plzpk_in AND "strasse" = l_v_strasse_in AND "hausnummer" = l_v_hausnummer_in;
+    IF l_n_countEntries > 0 THEN
+    SELECT "adresseid" INTO l_n_adresseid FROM "Adresse" WHERE "plz" = l_n_plzpk_in AND "strasse" = l_v_strasse_in AND "hausnummer" = l_v_hausnummer_in;
+        l_n_pk_out := l_n_adresseid;
+        RETURN;
+    ELSE
+        SELECT MAX("adresseid") INTO l_n_maxAdresseid FROM "Adresse";
+        INSERT INTO "Adresse" VALUES (l_n_maxAdresseid+1, l_n_plzpk_in, l_v_strasse_in, l_v_hausnummer_in);
+        l_n_pk_out := l_n_maxAdresseid+1;
+    END IF;
+EXCEPTION
+    WHEN others THEN
+        l_n_error_out := SQLCODE;
+END;
+/
+
+-- TODO:
+-- Adresse anlegen
+-- Rolle anlegen
 -- User anlegen
